a141f87f0dc661935032e217cc3b90d97436e102
tutai tran <ps234134@summacollege.nl>
2024-06-04 14:21:48 +0200

    seo verbeteringen

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,7 @@ export default defineNuxtConfig({
     }
   },
 
-  modules: ["@nuxt/ui", "@nuxtjs/color-mode"],
+  modules: ["@nuxt/ui", "@nuxtjs/color-mode", "@nuxtjs/sitemap", "@nuxtjs/seo"],
 
   colorMode: {
     preference: 'light', // default value of $colorMode.preference
