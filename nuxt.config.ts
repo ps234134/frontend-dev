d0c725b977548236b613c09e894eb689c9c0237a
tutai tran <ps234134@summacollege.nl>
2024-06-04 10:38:59 +0200

    final

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -13,6 +13,17 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  app:{
+    head:{
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { hid: 'description', name: 'description', content: 'EasyExercises is a platform designed to help individuals around the world reach their fitness goals. We provide detailed exercise instructions, customizable workout plans, and a supportive community to keep you motivated.' },
+        { hid: 'keywords', name: 'keywords', content: 'fitness, exercise, workout, health, wellness, community, motivation' },
+      ],
+      htmlAttrs: { lang: 'en' },
+    }
+  },
 
   modules: ["@nuxt/ui", "@nuxtjs/color-mode"],
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,10 @@
 <script setup>
+useHead({
+  title: "Home",
+});
 definePageMeta({
   layout: "landing",
   colorMode:"light",
-  title: "EasyExercises",
-  meta: [
-    {
-      name: "description",
-      content: "EasyExercises is a platform designed to help individuals around the world reach their fitness goals. We provide detailed exercise instructions, customizable workout plans, and a supportive community to keep you motivated."
-    },
-    {
-      name: "keywords",
-      content: "fitness, exercises, workout, health, community"
-    },
-  ],
 });
 </script>
 
